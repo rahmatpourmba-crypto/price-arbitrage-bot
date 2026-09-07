9bd45663640efc11a9480d7fda0784353a5f2c32
token factory pol <rahmatpourmba@gmail.com>
2026-09-07 12:44:19 +0330

    Add start_bot.bat and update README with startup instructions

---
+++ start_bot.bat
@@ -0,0 +1,6 @@
+@echo off
+title Telegram Arbitrage Bot
+cd /d "C:\Users\Admin\Documents\Default Project\price_arbitrage_bot"
+set PYTHONPATH=C:\Users\Admin\Documents\Default Project\price_arbitrage_bot
+python main.py
+pause
